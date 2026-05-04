0b7a2d01abcde2ac532265d9c0c710ad3c87b49c
Robby Riandi <rbyrndi@gmail.com>
2026-05-04 09:03:52 +0700

    routing request

--- server.js
+++ server.js
@@ -2,9 +2,38 @@ import http, { request } from 'http';
 
 const requestListener = (request, response) => {
     response.setHeader('Content-Type', 'text/html');
-
     response.statusCode = 200;
-    response.end('<h1>Hello, World!</h1>');
+
+    const { method, url } = request;
+
+    if (url === '/') {
+        if (method === 'GET') {
+            response.end('<h1>Ini adalah homepage</h1>');
+        } else {
+            response.end(`<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`);
+        }
+    } else if (url === '/about') {
+        if ( method === 'GET') {
+            response.end('<h1>Halo! Ini adalah halaman about</h1>');
+        } else if (method === 'POST') {
+            let body = [];
+
+            request.on('data', (chunk) => {
+                body.push(chunk);
+            });
+
+            request.on('end', () => {                
+                body = Buffer.concat(body).toString();
+                const { name } = JSON.parse(body);
+                response.end(`<h1>Halo, ${name}! Ini adalah halaman about</h1>`);
+            });
+        } else {
+            response.end(`<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`);
+        }
+
+    } else {
+        response.end('<h1>Halaman tidak ditemukan!</h1>');
+    }
 };
 
 const server = http.createServer(requestListener);
